86778c22bbc2c0a410ba72595553d366ea89916b
Konstantin <pec-orange@mail.ru>
2021-11-30 17:55:20 +0300

    anr question

---
+++ NKR_model/anr_test.m
@@ -0,0 +1,18 @@
+V = 5.236;
+lf = 0.4;
+lr = 0.4;
+lw = 0.6;
+wf = atan(lw / 2*lf);
+wr = atan(lw / 2*lr);
+Lr = sqrt(lr^2 + 0.25*lw^2);
+Lf = sqrt(lf^2 + 0.25*lw^2);
+
+gamma = 0.0799; gamma1 = gamma - gamma^2*lw/(lf+lr); gamma2 = gamma + gamma^2*lw/(lf+lr);
+
+Vxb = 0.25 * ( V*cos(gamma1) + V*cos(gamma2) + V + V);
+Vyb = 0.25 * ( V*sin(gamma1) + V*sin(gamma2));
+betta = atan(Vyb/Vxb);
+
+
+dpsi = 0.25*V*sin(gamma1-betta)/Lf/cos(wf+gamma1)-0.25*V*sin(gamma2-betta)/Lf/cos(wf-gamma2) + 0.25*V*sin(betta)/Lr/cos(-wr)+0.25*V*sin(betta)/Lr/cos(wr)
+dpsi2 = V*cos(betta)*tan(gamma) / (lf +lr)
--- NKR_model/ideal_model.m
+++ NKR_model/ideal_model.m
@@ -2,43 +2,49 @@ close all
 clear
 lf = 0.4;
 lr = 0.4;
-lw = 0.3;
-a = 0.05;
-rw = 0.1;
+lw = 0.6;
+a = 0.05; % nkr wheel scaling for drawing purpose
+rw = 0.15;
 dt = 0.01;
 simTime = 20;
 nSim = simTime / dt;
-Animation_enable = 0;
 
 wheel1.x = 0;
 wheel1.y = 0;
 wheel1.h = -pi/2;
 
-UGV.lf = 0.4;
-UGV.lr = 0.3;
-UGV.lw = 0.3;
-UGV.rw = 0.1;
 UGV.frame = [lf,0;lf,lw/2;lf+a,0;lf,-lw/2;lf,0; 0,0;0,0.02;0,-0.02; 0,0;-lr,0;-lr,-lw/2;-lr+a,0;-lr,lw/2; -lr, 0];
 Wheel.frame = [0.1,0.05; -0.1, 0.05; -0.1, -0.05; 0.1, -0.05; 0.1,0.05];
 %% Arrays init
 Time = zeros(nSim, 1);
+% NKR params
 V = zeros(nSim, 1);
 Anr = zeros(nSim, 1);
 Heading = zeros(nSim, 1);
 X = zeros(nSim, 1);
 Y = zeros(nSim, 1);
 w = zeros(nSim, 4);
+gamma = zeros(nSim, 2);
+betta = zeros(nSim, 5);
+% Measurements
 odo_w = zeros(nSim, 4);
 gps_pos = zeros(nSim, 2);
 gps_vel = zeros(nSim, 2);
 gyro_anr = zeros(nSim, 1);
-gamma = zeros(nSim, 6);
-betta = zeros(nSim, 1);
+
 odo_coords = zeros(nSim, 3);
 wheels = zeros(nSim, 8);
 % store navigation solution
-sol = zeros(nSim, 9);
+% sol = zeros(nSim, 9);
 % sol key: [X Y Heading V dHeading w1 w2 w3 w4]
+V1 = zeros(nSim, 1);
+V2 = zeros(nSim, 1);
+V3 = zeros(nSim, 1);
+V4 = zeros(nSim, 1);
+V11 = zeros(nSim, 1);
+V22 = zeros(nSim, 1);
+V33 = zeros(nSim, 1);
+V44 = zeros(nSim, 1);
 
 trajPhase = 1;
 trajConst = 1.91;
@@ -46,13 +52,8 @@ trajTimer = 1.91;
 
 odoX = 0;
 odoY = 0;
-odoHeading = -pi/2;
+odoHeading = 0;
 
-if Animation_enable == 1
-    anim = figure('Name', 'Robot motion animation');
-    axis equal
-    grid on
-end
 
 %% Measurement errors parameters
 odometer_error = 0.03; % percent (max error)
@@ -65,26 +66,26 @@ gyro_bias = normrnd(0, 0.1); % rad/s
 gyro_noise = 2*pi/180; % rad/s
 
 % init robot struct
-robot.x = 0;
-robot.y = 0;
-robot.heading = -pi/2;
-robot.v = 0;
-robot.dheading = 0;
-robot.lf = 0.4;
-robot.lr = 0.3;
-robot.lw = 0.3;
-robot.rw = 0.1;
-robot.lsx = 0.15; % gnss antenna shift
-robot.lsy = 0.05; % gnss antenna shift
+% robot.x = 0;
+% robot.y = 0;
+% robot.heading = 0;
+% robot.v = 0;
+% robot.dheading = 0;
+% robot.lf = 0.4;
+% robot.lr = 0.3;
+% robot.lw = 0.3;
+% robot.rw = 0.1;
+% robot.lsx = 0.15; % gnss antenna shift
+% robot.lsy = 0.05; % gnss antenna shift
 
 %init Kalman state struct
-kalman_state.X = zeros(7,1);
-kalman_state.P = diag([10 10 3 5 5 5 5]);
+% kalman_state.X = zeros(7,1);
+% kalman_state.P = diag([10 10 3 5 5 5 5]);
 
 %% Main cycle
 for i = 1:nSim
     Time(i) = i * dt;
-    V(i) = 5.236;
+    V(i) = 5.236; % constant velocity
     %% Motion setup
     if Time(i) >= trajTimer
         trajPhase = trajPhase + 1;
@@ -93,51 +94,56 @@ for i = 1:nSim
         else
             trajTimer = trajTimer + 2*trajConst; % 3.82 seconds for straight line
         end
+        if rem(trajPhase, 5) == 0
+%             V(i) = V(i) - 1;
+        end
     end
-    if rem(trajPhase, 2) ~= 0
+    gamma_mean = 0;
+    if rem(trajPhase, 2) ~= 0 % odd phase
         Anr(i) = 0;
-        gamma(i,:) = [0, 0, 0, 0, 0, 0];
-    else
+        gamma(i,:) = [0, 0];
+    else                        % even phase
         if rem(trajPhase, 4) == 2
-            Anr(i) = pi/6; % turn right on every 2nd phase
-            gamma(i,:) = [0, 0, 0, 0, 0, 0];
+%             Anr(i) = pi/6; % turn right on every 2nd phase
+            gamma_mean = 0.0799;% will be equal to 0.5236 rad/s
+            gamma(i,1) = gamma_mean - gamma_mean^2*lw/(lf+lr);
+            gamma(i,2) = gamma_mean + gamma_mean^2*lw/(lf+lr);
         else
-            Anr(i) = -pi/6; % turn left on every 4th phase
-            gamma(i,:) = [0, 0, 0, 0, 0, 0];
+%             Anr(i) = -pi/6; % turn left on every 4th phase
+            gamma_mean = -0.0799; % will be equal to -0.5236 rad/s
+            gamma(i,1) = gamma_mean - gamma_mean^2*lw/(lf+lr);
+            gamma(i,2) = gamma_mean + gamma_mean^2*lw/(lf+lr);
         end
     end
     %% Wheels kinematics
     % Linear speed of each wheel
-    V1 = V(i); V2 = V(i); V3 = V(i); V4 = V(i);
+    V1(i) = V(i); V2(i) = V(i); V3(i) = V(i); V4(i) = V(i);
     A1 = 0; A2 = 0; A3 = 0; A4 = 0;
+    
+     
+    
+    Vxb = 0.25 * ( V(i)*cos(gamma(i,1)) + V(i)*cos(gamma(i,2)) + V(i) + V(i));
+    Vyb = 0.25 * ( V(i)*sin(gamma(i,1)) + V(i)*sin(gamma(i,2)));
+    betta(i) = atan(Vyb/Vxb);
+    Anr(i) = V(i)*cos(betta(i))*tan(gamma_mean) / (lf + lr);
     
     % If robot is turning - they differ
     if rem(trajPhase, 2) == 0
-        V1 = abs(Anr(i)) * ( 10 + lw/2 * sign(Anr(i)) );
-        V2 = abs(Anr(i)) * ( 10 - lw/2 * sign(Anr(i)) );
-        V3 = abs(Anr(i)) * ( 10 + lw/2 * sign(Anr(i)) );
-        V4 = abs(Anr(i)) * ( 10 - lw/2 * sign(Anr(i)) );
-    end   
-    
-    betta(i) = 0; % assume that betta is quasi-constant
-    % Also assume that gamma_f equals gamma_r, but has the opposite sign
-    gamma(i,5) =  atan(Anr(i)*(lf + lr) / (V(i)*cos(betta(i))) / 2);
-    gamma(i,6) = - gamma(i,5);
-    dgammaf = gamma(i,5)^2 * lw / (2*lf + 2*lr);
-    dgammar = gamma(i,6)^2 * lw / (2*lf + 2*lr);
-    gamma(i,1) = gamma(i,5) + dgammaf*sign(gamma(i,5));
-    gamma(i,2) = gamma(i,5) - dgammaf*sign(gamma(i,5));
-    gamma(i,3) = gamma(i,6) + dgammar*sign(gamma(i,6));
-    gamma(i,4) = gamma(i,6) - dgammar*sign(gamma(i,6));
+        V1(i) = abs(Anr(i)) * ( 10 + lw/2 * sign(Anr(i)) * cos(gamma_mean) );
+        V2(i) = abs(Anr(i)) * ( 10 - lw/2 * sign(Anr(i)) * cos(gamma_mean));
+        V3(i) = abs(Anr(i)) * ( 10 + lw/2 * sign(Anr(i)) * cos(gamma_mean));
+        V4(i) = abs(Anr(i)) * ( 10 - lw/2 * sign(Anr(i)) * cos(gamma_mean));
+    end  
+
     %% True model parameters
     if i == 1
         X(i) = 0;
         Y(i) = 0;
-        Heading(i) = -pi/2;
+        Heading(i) = 0;
     else
         Heading(i) = Heading(i-1) + Anr(i)*dt;
-        X(i) = X(i-1) + V(i)*cos(Heading(i-1) + betta(i-1))*dt;
-        Y(i) = Y(i-1) + V(i)*sin(Heading(i-1) + betta(i-1))*dt;
+        X(i) = X(i-1) + V(i)*cos(Heading(i) + betta(i))*dt;
+        Y(i) = Y(i-1) + V(i)*sin(Heading(i) + betta(i))*dt;
     end  
     %% True Wheel parameters
     wheels(i,1) = X(i) + lf*cos(Heading(i)) + lw/2*sin(Heading(i));
@@ -149,16 +155,16 @@ for i = 1:nSim
     wheels(i,7) = X(i) - lr*cos(Heading(i)) - lw/2*sin(Heading(i));
     wheels(i,8) = Y(i) - lr*sin(Heading(i)) + lw/2*cos(Heading(i));
     if i ~= 1
-        V1 = sqrt( (wheels(i,1) - wheels(i-1,1))^2 + (wheels(i,2) - wheels(i-1,2))^2 )/dt;
-        V2 = sqrt( (wheels(i,3) - wheels(i-1,3))^2 + (wheels(i,4) - wheels(i-1,4))^2 )/dt;
-        V3 = sqrt( (wheels(i,5) - wheels(i-1,5))^2 + (wheels(i,6) - wheels(i-1,6))^2 )/dt;
-        V4 = sqrt( (wheels(i,7) - wheels(i-1,7))^2 + (wheels(i,8) - wheels(i-1,8))^2 )/dt;
+        V11(i) = sqrt( (wheels(i,1) - wheels(i-1,1))^2 + (wheels(i,2) - wheels(i-1,2))^2 )/dt;
+        V22(i) = sqrt( (wheels(i,3) - wheels(i-1,3))^2 + (wheels(i,4) - wheels(i-1,4))^2 )/dt;
+        V33(i) = sqrt( (wheels(i,5) - wheels(i-1,5))^2 + (wheels(i,6) - wheels(i-1,6))^2 )/dt;
+        V44(i) = sqrt( (wheels(i,7) - wheels(i-1,7))^2 + (wheels(i,8) - wheels(i-1,8))^2 )/dt;
     end
-    w(i,:) = [V1/rw, V2/rw, V3/rw, V4/rw];
+    w(i,:) = [V1(i)/rw, V2(i)/rw, V3(i)/rw, V4(i)/rw];
     aw1(i) = V(i)/rw;
     if rem(trajPhase, 2) == 0
-        V1 = abs(Anr(i)) * sqrt(lf^2 + (10+sign(Anr(i))*lw/2)^2); % 
-        aw1(i) = V1/rw;
+        V123 = abs(Anr(i)) * sqrt(lf^2 + (10+sign(Anr(i))*lw/2)^2); % 
+        aw1(i) = V123/rw;
     end
     %% Measurements model
     % Odometer errors
@@ -187,53 +193,45 @@ for i = 1:nSim
     % Gyroscope errors
     gyro_anr(i) = Anr(i) + gyro_bias + normrnd(0, gyro_noise);
     %% Baseline model
-    odoV = rw/4 * (odo_w(i,1) + odo_w(i,2) + odo_w(i,3) + odo_w(i,4));
-    odoBetta = atan( (lf*tan(gamma(i,6)) + lr*tan(gamma(i,5)))/(lf + lr) );
-    odoAnr = odoV * cos(odoBetta) / (lr + lf) * (tan(gamma(i,5)) - tan(gamma(i,6)));
-    odoHeading = odoHeading + odoAnr*dt;
-    odoX = odoX + odoV*cos(odoHeading + odoBetta)*dt;
-    odoY = odoY + odoV*sin(odoHeading + odoBetta)*dt;
-    odo_coords(i,:) = [odoX, odoY, odoHeading];
-    update.gps = 0;
-    update.gyro = 0;
-    if (rem(i,1) == 0)
-        update.gyro = 1;
-    end
-    if (rem(i, 100) == 0)
-        update.gps = 1;
-    end
-    % init sensors struct
-    sensors.gps_error_pos = 2.0;
-    sensors.gps_error_vel = 0.2;
-    sensors.gyro_error = 2*pi/180;
-    sensors.dt = dt;
-    sensors.gps_x = gps_pos(i,1);
-    sensors.gps_y = gps_pos(i,2);
-    sensors.gps_dx = gps_vel(i,1);
-    sensors.gps_dy = gps_vel(i,2);
-    sensors.gyro = gyro_anr(i);
-    % update robot struct
-    robot.w1 = odo_w(i,1);
-    robot.w2 = odo_w(i,2);
-    robot.w3 = odo_w(i,3);
-    robot.w4 = odo_w(i,4);
-    robot.betta = odoBetta;
-    robot.gammaf = gamma(i,6);
-    robot.gammar = gamma(i,5);
+%     odoV = rw/4 * (odo_w(i,1) + odo_w(i,2) + odo_w(i,3) + odo_w(i,4));
+%     odoBetta = atan( (lf*tan(gamma(i,6)) + lr*tan(gamma(i,5)))/(lf + lr) );
+%     odoAnr = odoV * cos(odoBetta) / (lr + lf) * (tan(gamma(i,5)) - tan(gamma(i,6)));
+%     odoHeading = odoHeading + odoAnr*dt;
+%     odoX = odoX + odoV*cos(odoHeading + odoBetta)*dt;
+%     odoY = odoY + odoV*sin(odoHeading + odoBetta)*dt;
+%     odo_coords(i,:) = [odoX, odoY, odoHeading];
+%     update.gps = 0;
+%     update.gyro = 0;
+%     if (rem(i,1) == 0)
+%         update.gyro = 1;
+%     end
+%     if (rem(i, 100) == 0)
+%         update.gps = 1;
+%     end
+%     % init sensors struct
+%     sensors.gps_error_pos = 2.0;
+%     sensors.gps_error_vel = 0.2;
+%     sensors.gyro_error = 2*pi/180;
+%     sensors.dt = dt;
+%     sensors.gps_x = gps_pos(i,1);
+%     sensors.gps_y = gps_pos(i,2);
+%     sensors.gps_dx = gps_vel(i,1);
+%     sensors.gps_dy = gps_vel(i,2);
+%     sensors.gyro = gyro_anr(i);
+%     % update robot struct
+%     robot.w1 = odo_w(i,1);
+%     robot.w2 = odo_w(i,2);
+%     robot.w3 = odo_w(i,3);
+%     robot.w4 = odo_w(i,4);
+%     robot.betta = odoBetta;
+%     robot.gammaf = gamma(i,6);
+%     robot.gammar = gamma(i,5);
     % call Kalman dilter
 %     [robot, kalman_state] = navSys(robot, kalman_state, sensors, update);
 %     sol(i,:) = [robot.x robot.y robot.heading robot.v robot.dheading robot.w1 robot.w2 robot.w3 robot.w4];
     % sol key: [X Y Heading V dHeading w1 w2 w3 w4]
     %% Animation
-    if Animation_enable == 1
-        trail = plot(Y(1:i),X(1:i), 'Color', [0.4 0.4 0.4], 'LineWidth', 0.5);
-        UGVframe = rotateVectorImage2D(UGV.frame, Heading(i));
-        frame = drawVectorImage2D(UGVframe, [Y(i) X(i)], anim, 'r');
-        drawnow;
-        delete(frame);
-        delete(trail);
 
-    end
 end
 
 
@@ -251,21 +249,23 @@ plot(wheels(:,2), wheels(:,1), 'g', 'LineWidth', 0.25)
 axis equal
 grid on
 
-UGVframe = rotateVectorImage2D(UGV.frame, Heading(end));
-frame1 = drawVectorImage2D(UGVframe, [Y(end) X(end)], an, 'r');
-Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,1));
-frame2 = drawVectorImage2D(Wheelframe, [wheels(end,2) wheels(end,1)], an, 'm');
-Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,2));
-frame3 = drawVectorImage2D(Wheelframe, [wheels(end,4) wheels(end,3)], an, 'm');
-Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,3));
-frame4 = drawVectorImage2D(Wheelframe, [wheels(end,6) wheels(end,5)], an, 'm');
-Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,4));
-frame5 = drawVectorImage2D(Wheelframe, [wheels(end,8) wheels(end,7)], an, 'm');
+% UGVframe = rotateVectorImage2D(UGV.frame, Heading(end));
+% frame1 = drawVectorImage2D(UGVframe, [Y(end) X(end)], an, 'r');
+% Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,1));
+% frame2 = drawVectorImage2D(Wheelframe, [wheels(end,2) wheels(end,1)], an, 'm');
+% Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,2));
+% frame3 = drawVectorImage2D(Wheelframe, [wheels(end,4) wheels(end,3)], an, 'm');
+% Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,3));
+% frame4 = drawVectorImage2D(Wheelframe, [wheels(end,6) wheels(end,5)], an, 'm');
+% Wheelframe = rotateVectorImage2D(Wheel.frame, Heading(end) + gamma(end,4));
+% frame5 = drawVectorImage2D(Wheelframe, [wheels(end,8) wheels(end,7)], an, 'm');
 
 figure
-plot(Time, w(:,1), 'b', 'LineWidth', 0.5)
+plot(Time, V1, 'b', 'LineWidth', 0.5)
 hold on; grid on
-plot(Time, aw1, 'r', 'LineWidth', 0.5)
+plot(Time, V2, 'r', 'LineWidth', 0.5)
+plot(Time, V11, '--b', 'LineWidth', 0.5)
+plot(Time, V22, '--r', 'LineWidth', 0.5)
 % 
 % figure;
 % plot(Time, w, 'b')
