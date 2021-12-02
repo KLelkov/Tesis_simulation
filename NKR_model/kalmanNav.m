c32ecd298092d83c2537eddb0cbaade6d3cbbd14
KLelkov <pec-orange@mail.ru>
2021-12-02 16:59:58 +0300

    [wip] kalman measurement and param update

--- NKR_model/kalmanNav.m
+++ NKR_model/kalmanNav.m
@@ -82,256 +82,62 @@ function state_prime = kalmanNav(state, sensors, params)
     Pprime = F * state.P * F' + Q;
     
     %% Kalamn Correction GYRO
-    
-    %% Kalman Correction ODO
-    
+    if (sensors.gyro_update == 1)
+        Z = [avs_dpsi - dpsi];
+        H = zeros(1,16);
+        H(5) = -1;
+        R = [sensros.egyro];
+        I = eye(16);
+        Y = Z - H * Xprime;
+        S = H * P * H' + R;
+        K = P * H' * inv(S);
+        Xprime = Xprime + K * Y;
+        Pprime = (I - K * H) * Pprime;
+    end
     
     %% Kalman Correction GPS
-    
-    %% Prediction
-    % Motion transform (apply system dynamic F)
-
-
-    % Степень доверия предсказаниям (чем меньше коэф. - тем больше ему
-    % доверяем)
-    
-    
-    % Apply prediction update to every column of Xs
-    for j = 1:n
-        Xs(1,j) = Xs(1,j) + Xs(4,j)*cos(Xs(3,j))*dt; % X
-        Xs(2,j) = Xs(2,j) + Xs(4,j)*sin(Xs(3,j))*dt; % Y
-        Xs(3,j) = Xs(3,j) + Xs(5,j)*dt; % Heading
-        Xs(4,j) = rw / 4 * ( Xs(6,j) + Xs(7,j) + Xs(8,j) + Xs(9,j) ); % V
-        Xs(5,j) = (tangf-tangr)/(lf+lr) * Xs(4,j); % dHeading
-%         Xs(5,j) = rw / (2 * lw) * (Xs(6,j) - Xs(7,j) + Xs(8,j) - Xs(9,j));
-        Xs(6,j) = Xs(6,j); % w1
-        Xs(7,j) = Xs(7,j); % w2
-        Xs(8,j) = Xs(8,j); % w3
-        Xs(9,j) = Xs(9,j); % w4
+    if (sensors.gps_update == 1)
+        Z = [gps_x - x - lsx * cos(psi) + lsy + sin(psi);
+            gps_y - y - lsx * sin(psi) - lsy * cos(psi);
+            gps_dx - vel(1);
+            gps_dy - vel(2)];
+        R = [sensors.egps_pos, sensors.egps_pos, sensors.egps_vel, sensors.egps_vel];
+        H = zeros(4,16);
+        H(1:2,1:2) = eye(2);
+        H(1,5) = -(lsx * sin(psi) + lsy * cos(psi));
+        H(2,5) = lsx * cos(psi) - lsy * sin(psi);
+        H(3:4,3:4) = eye(2);
+        H(3,5) = - (dpsi * lsx * sinz(psi) + dpsi * lsy * cos(psi));
+        H(3,6) = lsx * cos(psi) - lsy * sin(psi);
+        H(4,5) = dpsi * lsx * cos(psi) - dpsi * lsy * sin(psi);
+        H(4,6) = lsx * sin(psi) + lsy * cos(psi);
+        I = eye(16);
+        Y = Z - H * Xprime;
+        S = H * P * H' + R;
+        K = P * H' * inv(S);
+        Xprime = Xprime + K * Y;
+        Pprime = (I - K * H) * Pprime;
     end
-    
-    %% Reverse unscented transform
-    % Derive Xprime as weighted mean
-    for i = 1:dim
-        Xprime(i) = 0;
-        for j = 1:n
-            Xprime(i) = Xprime(i) + Xs(i,j)*Ws(1,j);
-        end
-    end
-    % Derive Pprime as weighted covariance
-    for j = 1:n
-        disp(j)
-        a = Ws(2,j) * ((Xs(:,j) - Xprime) * (Xs(:,j) - Xprime)');
-        disp(a)
-        Pprime = Pprime + a;
-    end
-    Pprime = Pprime + diag(U);
-
-
-    P = Pprime;
-    X = Xprime;
-    
-    %% Correction
-%     
-%     % Apply H to Xs to get predicted measurement Z
-%     if update.gyro == 1 && update.gps == 1
-%         dimz = 9;
-%         Zs = zeros(dimz, n);
-%         Zhat = zeros(dimz, 1);
-%         Z = [sensors.gps_x;
-%                 sensors.gps_y;
-%                 sensors.gps_dx;
-%                 sensors.gps_dy;
-%                 sensors.gyro;
-%                 sensors.w1;
-%                 sensors.w2;
-%                 sensors.w3;
-%                 sensors.w4];
-%         % Дисперсии ошибок датчиков (паспортные данные)
-%         R = diag([sensors.gps_error_pos sensors.gps_error_pos sensors.gps_error_vel sensors.gps_error_vel sensors.gyro_error sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-%         for j = 1:n
-%             Zs(1,j) = Xs(1,j) + lsx*cos(Xs(3,j)) - lsy*sin(Xs(3,j));
-%             Zs(2,j) = Xs(2,j) + lsx*sin(Xs(3,j)) + lsy*cos(Xs(3,j));
-%             Zs(3,j) = Xs(4,j)*cos(Xs(3,j)) + Xs(5,j)*(lsx*cos(Xs(3,j)) - lsy*sin(Xs(3,j)));
-%             Zs(4,j) = Xs(4,j)*sin(Xs(3,j)) + Xs(5,j)*(lsx*sin(Xs(3,j)) + lsy*cos(Xs(3,j)));
-%             Zs(5,j) = Xs(5,j);
-%             Zs(6,j) = Xs(6,j);
-%             Zs(7,j) = Xs(7,j);
-%             Zs(8,j) = Xs(8,j);
-%             Zs(9,j) = Xs(9,j);
-%         end
-%     elseif update.gps == 1 % never happens (gps always comes with gyro)
-%         dimz = 8;
-%         Zs = zeros(dimz, n);
-%         Zhat = zeros(dimz, 1);
-%         Z = [sensors.gps_x;
-%                 sensors.gps_y;
-%                 sensors.gps_dx;
-%                 sensors.gps_dy;
-%                 sensors.w1;
-%                 sensors.w2;
-%                 sensors.w3;
-%                 sensors.w4];
-%         % Дисперсии ошибок датчиков (паспортные данные)
-%         R = diag([sensors.gps_error_pos sensors.gps_error_pos sensors.gps_error_vel sensors.gps_error_vel sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-%         for j = 1:n
-%             Zs(1,j) = Xs(1,j) + lsx*cos(Xs(3,j)) - lsy*sin(Xs(3,j));
-%             Zs(2,j) = Xs(2,j) + lsx*sin(Xs(3,j)) + lsy*cos(Xs(3,j));
-%             Zs(3,j) = Xs(4,j)*cos(Xs(3,j)) + Xs(5,j)*(lsx*cos(Xs(3,j)) - lsy*sin(Xs(3,j)));
-%             Zs(4,j) = Xs(4,j)*sin(Xs(3,j)) + Xs(5,j)*(lsx*sin(Xs(3,j)) + lsy*cos(Xs(3,j)));
-%             Zs(5,j) = Xs(6,j);
-%             Zs(6,j) = Xs(7,j);
-%             Zs(7,j) = Xs(8,j);
-%             Zs(8,j) = Xs(9,j);
-%         end
-%     elseif update.gyro == 1
-%         dimz = 5;
-%         Zs = zeros(dimz, n);
-%         Zhat = zeros(dimz, 1);
-%         Z = [ sensors.gyro;
-%                 sensors.w1;
-%                 sensors.w2;
-%                 sensors.w3;
-%                 sensors.w4];
-%         % Дисперсии ошибок датчиков (паспортные данные)
-%         R = diag([sensors.gyro_error sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-%         for j = 1:n
-%             Zs(1,j) = Xs(5,j);
-%             Zs(2,j) = Xs(6,j);
-%             Zs(3,j) = Xs(7,j);
-%             Zs(4,j) = Xs(8,j);
-%             Zs(5,j) = Xs(9,j);
-%         end
-%     else
-%         dimz = 4;
-%         Zs = zeros(dimz, n);
-%         Zhat = zeros(dimz, 1);
-%         Z = [ sensors.w1;
-%                 sensors.w2;
-%                 sensors.w3;
-%                 sensors.w4];
-%         % Дисперсии ошибок датчиков (паспортные данные)
-%         R = diag([sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-%         for j = 1:n
-%             Zs(1,j) = Xs(6,j);
-%             Zs(2,j) = Xs(7,j);
-%             Zs(3,j) = Xs(8,j);
-%             Zs(4,j) = Xs(9,j);
-%         end
-%     end
-%     
-%     % Derive Zhat
-%     for i = 1:dimz
-%         Zhat(i) = 0;
-%         for j = 1:n
-%             Zhat(i) = Zhat(i) + Zs(i,j)*Ws(1,j);
-%         end
-%     end
-%     
-%     % Matrix S
-%     S = zeros(dimz, dimz);
-%     for j = 1:n
-%         S = S + Ws(2,j) * ((Zs(:,j) - Zhat) * (Zs(:,j) - Zhat)');
-%     end
-%     S = S + diag(R);
-%     
-%     % Matrix Sigma (x,z)
-%     Sig = zeros(dim, dimz);
-%     for j = 1:n
-%         Sig = Sig + Ws(2,j) * ((Xs(:,j) - Xprime) * (Zs(:,j) - Zhat)');
-%     end
-%     K = Sig*inv(S);
-%     Xprime = X + K*(Z - Zhat);
-%     Pprime = P - K*S*K';
-    
-%%
-    heading = X(3);
-    xproj = lsx*cos(heading) - lsy*sin(heading);
-    yproj = lsx*sin(heading) + lsy*cos(heading);
-    if update.gyro == 1 && update.gps == 1
-        %     X  Y  H.            V     dH. w1 w2 w3 w4
-        H = [ 1, 0, 0,            0,     0, 0, 0, 0, 0; % Xs
-              0, 1, 0,            0,     0, 0, 0, 0, 0; % Ys
-              0, 0, 0, cos(heading), xproj, 0, 0, 0 ,0; % dXs
-              0, 0, 0, sin(heading), yproj, 0, 0, 0, 0; % dYs
-              0, 0, 0,            0,     1, 0, 0, 0, 0; % dHeading_gyro
-              0, 0, 0,            0,     0, 1, 0, 0, 0; % odo1
-              0, 0, 0,            0,     0, 0, 1, 0, 0; % odo2
-              0, 0, 0,            0,     0, 0, 0, 1, 0; % odo3
-              0, 0, 0,            0,     0, 0, 0, 0, 1];% odo4
-
-        Z = [sensors.gps_x - xproj;
-            sensors.gps_y - yproj;
-            sensors.gps_dx;
-            sensors.gps_dy;
-            sensors.gyro;
-            sensors.w1;
-            sensors.w2;
-            sensors.w3;
-            sensors.w4];
-        % Дисперсии ошибок датчиков (паспортные данные)
-        R = diag([sensors.gps_error_pos sensors.gps_error_pos sensors.gps_error_vel sensors.gps_error_vel sensors.gyro_error sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-    elseif update.gyro == 1
-        %     X  Y     H.             V    dH. w1 w2 w3 w4
-        H = [ 0, 0,     0,            0,     1, 0, 0, 0, 0; % dHeading_gyro
-              0, 0,     0,            0,     0, 1, 0, 0, 0; % odo1
-              0, 0,     0,            0,     0, 0, 1, 0, 0; % odo2
-              0, 0,     0,            0,     0, 0, 0, 1, 0; % odo3
-              0, 0,     0,            0,     0, 0, 0, 0, 1];% odo4
-
-        Z = [sensors.gyro;
-            sensors.w1;
-            sensors.w2;
-            sensors.w3;
-            sensors.w4];
-        % Дисперсии ошибок датчиков (паспортные данные)
-        R = diag([sensors.gyro_error sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-    elseif update.gps == 1 % never happens (gps always comes with gyro)
-        %     X  Y  H.            V     dH. w1 w2 w3 w4
-        H = [ 1, 0, 0,            0,     0, 0, 0, 0, 0; % Xs
-              0, 1, 0,            0,     0, 0, 0, 0, 0; % Ys
-              0, 0, 0, cos(heading), xproj, 0, 0, 0 ,0; % dXs
-              0, 0, 0, sin(heading), yproj, 0, 0, 0, 0; % dYs
-              0, 0, 0,            0,     0, 1, 0, 0, 0; % odo1
-              0, 0, 0,            0,     0, 0, 1, 0, 0; % odo2
-              0, 0, 0,            0,     0, 0, 0, 1, 0; % odo3
-              0, 0, 0,            0,     0, 0, 0, 0, 1];% odo4
-
-        Z = [sensors.gps_x - xproj;
-            sensors.gps_y - yproj;
-            sensors.gps_dx;
-            sensors.gps_dy;
-            sensors.w1;
-            sensors.w2;
-            sensors.w3;
-            sensors.w4];
-        % Дисперсии ошибок датчиков (паспортные данные)
-        R = diag([sensors.gps_error_pos sensors.gps_error_pos sensors.gps_error_vel sensors.gps_error_vel sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-    else % only odometry is available (may happen if gyro measurements dont come at every simulation step)
-        %     X  Y     H.             V    dH. w1 w2 w3 w4
-        H = [ 0, 0,     0,            0,     0, 1, 0, 0, 0; % odo1
-              0, 0,     0,            0,     0, 0, 1, 0, 0; % odo2
-              0, 0,     0,            0,     0, 0, 0, 1, 0; % odo3
-              0, 0,     0,            0,     0, 0, 0, 0, 1];% odo4
-
-        Z = [sensors.w1;
-            sensors.w2;
-            sensors.w3;
-            sensors.w4];
-        % Дисперсии ошибок датчиков (паспортные данные)
-        R = diag([sensors.odo_error sensors.odo_error sensors.odo_error sensors.odo_error]);
-    end
-    
-    
-    y = Z - H*X; % невязка
-    S = H * P * H' + R; 
-    K = P * H' * inv(S);
-    Xprime = X + K*y; % X'
-    Pprime = (eye(9) - K*H)*P; % P'
     
     state_prime = state;
     state_prime.P = Pprime;
     state_prime.X = Xprime;
     
+    params.x = params.x - Xprime(1);
+    params.y = params.y - Xprime(2);
+    params.vel(1) = Vxb - Xprime(3);
+    params.vel(2) = Vyb - Xprime(4);
+    params.heading = params.heading + params.angular_rate * dt - Xprime(5);
+    params.angular_rate = V * cos(B) * tan(0.5 * (gamma(1) + gamma(2))) / (lr + lf) - Xprime(6);
+    params.gamma(1) = odo_gamma - odo_gamma^2 * lw / (lf + lr) - Xprime(7);
+    params.gamma(2) = odo_gamma + odo_gamma^2 * lw / (lf + lr) - Xprime(8);
+    params.omega(1) = odo_omega(1) - Xprime(9);
+    params.omega(2) = odo_omega(2) - Xprime(10);
+    params.omega(3) = odo_omega(3) - Xprime(11);
+    params.omega(4) = odo_omega(4) - Xprime(12);
+    params.betta(1) = params.betta(1) - Xprime(13);
+    params.betta(2) = params.betta(2) - Xprime(14);
+    params.betta(3) = params.betta(3) - Xprime(15);
+    params.betta(4) = params.betta(4) - Xprime(16);
 end
 
