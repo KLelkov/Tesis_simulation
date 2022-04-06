fc63d301bbc739cfa89bae1deeab5cbae405757d
pec-orange <pec-orange@mail.ru>
2022-04-06 18:29:59 +0300

    [wip] uav ideal trajectory. Linear motion in geo frame is OK, angular motion - not so much...

--- UAV_model/uav_trajectory_motion.m
+++ UAV_model/uav_trajectory_motion.m
@@ -1,8 +1,8 @@
 clear
 close all
 % X Y Z Xpoi Ypoi
-target_points = [0, -2.5, 3, 0, 3; % 1
-    0, -2, 5, 0, 3; % 1
+target_points = [0, 0, 3, 0, 3; % 1
+    0, 0, 5, 0, 3; % 1
     -4.5, 2, 5, 0, 3; % 1
     -4.5, 7, 5, 0, 7; % 2
     -3.5, 10, 5, 0, 12; % 3
@@ -49,7 +49,7 @@ target_points = [0, -2.5, 3, 0, 3; % 1
     3.5, 10, 5, 0, 12; % 3
     4.5, 7, 5, 0, 7; % 2
     4.5, 2, 5, 0, 3; % 1
-    0, -2.5, 3, 0, 3; % 1
+    0, 0, 3, 0, 3; % 1
     0, 0, 0, 0, 3; % 1
     ];
 
@@ -63,13 +63,20 @@ target_points(:,2) = temp1;
 target_points(:,4) = temp4;
 target_points(:,5) = temp3;
 
-uav_vel = 1.0/ sqrt(3); % m/s
-uav_rate = 1.0; % rad/s
-dt = 0.1; % sec
+uav_vel = 1.0 / sqrt(3); % m/s
+uav_rate = 5.0; % rad/s
+uav_acc_lim = 1.4; % m/s
+uav_rcc_lim = 4; % rad/s
+dt = 0.05; % sec
 X = 0;
 Y = 0;
 Z = 0;
+Velocity(1,:) = [0, 0, 0];
+Acceleration(1,:) = [0, 0, 0];
+Acceleration_b(1,:) = [0, 0, 0];
 Heading = 0;
+dHeading = 0;
+ddHeading = 0;
 Time = 0;
 i = 1;
 pnt = 1;
@@ -81,18 +88,22 @@ while ~fin
     dz = target_points(pnt,3) - Z(end);
     dx1 = target_points(pnt,4) - X(end);
     dy1 = target_points(pnt,5) - Y(end);
-    target_heading = atan2(dy1, dx1);
-    dh = target_heading - Heading(end);
+%     target_heading = atan2(dy1, dx1);
+%     dh = target_heading - Heading(end);
+    dh =  atan2(dy, dx) + pi/2 - Heading(end);
+    if dx == 0 && dy == 0
+        dh =  0 - Heading(end);
+    end
     
     if abs(dh) > pi
         dh = dh - 2*pi*sign(dh);
     end
     
-    dx = bound(3*dx, -uav_vel, uav_vel);
-    dy = bound(3*dy, -uav_vel, uav_vel);
-    dz = bound(3*dz, -uav_vel, uav_vel);
+    dx = bound(2*dx, -uav_vel, uav_vel);
+    dy = bound(2*dy, -uav_vel, uav_vel);
+    dz = bound(2*dz, -uav_vel, uav_vel);
     dh = bound(2*dh, -uav_rate, uav_rate);
-    
+%     dh = 1;
     if sqrt(dx^2 + dy^2 + dz^2) < 0.2
         pnt = pnt + 1;
         if pnt > 50
@@ -100,11 +111,45 @@ while ~fin
         end
     end
     
+    
+    Acceleration(i,1) = (dx - Velocity(end,1)) * 1.2 + Acceleration(i-1,1)*0.4;
+    Acceleration(i,2) = (dy - Velocity(end,2)) * 1.2 + Acceleration(i-1,2)*0.4;
+    Acceleration(i,3) = (dz - Velocity(end,3)) * 1.2 + Acceleration(i-1,3)*0.4;
+    Acceleration(i,1) = bound(Acceleration(i,1), -uav_acc_lim, uav_acc_lim);
+    Acceleration(i,2) = bound(Acceleration(i,2), -uav_acc_lim, uav_acc_lim);
+    Acceleration(i,3) = bound(Acceleration(i,3), -uav_acc_lim, uav_acc_lim);
+ 
+    ddHeading(i) = (dh - dHeading(end)) * 1.4 + ddHeading(i-1) * 0.6;
+    ddHeading(i) = bound(ddHeading(i), -uav_rcc_lim, uav_rcc_lim);
+     
     Time(i) = Time(i-1) + dt;
-    X(i) = X(i-1) + dx*dt;
-    Y(i) = Y(i-1) + dy*dt;
-    Z(i) = Z(i-1) + dz*dt;
-    Heading(i) = Heading(i-1) + dh*dt;
+    X(i) = X(i-1) + Velocity(i-1,1)*dt;
+    Y(i) = Y(i-1) + Velocity(i-1,2)*dt;
+    Z(i) = Z(i-1) + Velocity(i-1,3)*dt;
+    Velocity(i,1) = Velocity(i-1,1) + Acceleration(i-1,1) * dt;
+    Velocity(i,2) = Velocity(i-1,2) + Acceleration(i-1,2) * dt;
+    Velocity(i,3) = Velocity(i-1,3) + Acceleration(i-1,3) * dt;
+    Heading(i) = Heading(i-1) + dHeading(i-1) * dt;
+    dHeading(i) = dHeading(i-1) + ddHeading(i-1) * dt;
+    
+    
+%     Time(i) = Time(i-1) + dt;
+%     X(i) = X(i-1) + dx*dt;
+%     Y(i) = Y(i-1) + dy*dt;
+%     Z(i) = Z(i-1) + dz*dt;
+% %     Heading(i) = atan2(dy, dx) + pi/2;
+%     Heading(i) = Heading(i-1) + dh * dt;
+%     
+% %     dHeading(i) = (Heading(i) - Heading(i-1)) / dt;
+%     dHeading(i) = dh;
+%     Velocity(i,:) = [dx, dy, dz];
+%     Acceleration(i,1) = (Velocity(i,1) - Velocity(i-1,1)) / dt;
+%     Acceleration(i,2) = (Velocity(i,2) - Velocity(i-1,2)) / dt;
+%     Acceleration(i,3) = (Velocity(i,3) - Velocity(i-1,3)) / dt;
+%     
+%     Acceleration_b(i,1) = Acceleration(i,1) * cos(Heading(i)) + Acceleration(i,2) * sin(Heading(i));
+%     Acceleration_b(i,2) = -Acceleration(i,1) * sin(Heading(i)) + Acceleration(i,2) * cos(Heading(i));
+% %     Acceleration_b(i,3) = (Velocity(i,3) - Velocity(i-1,3)) / dt;
     
     if abs(Heading(i)) > pi
         Heading(i) = Heading(i) - 2*pi*sign(Heading(i));
@@ -134,4 +179,8 @@ for i = 1:len
 %     drawnow;
 %     pause(0.1);
 end
+
+figure;plot(Heading)
+figure;plot(dHeading)
+% figure;plot(Heading)
 
